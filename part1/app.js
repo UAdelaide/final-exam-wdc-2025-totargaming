f62a5e69f5c6d201917eb34026010f97b0c62262
elvistran <elvistranhere>
2025-06-20 02:25:19 +0000

    [automatic commit] 2025-06-20 02:25:19 UTC 193.115.102.250

--- part1/app.js
+++ part1/app.js
@@ -41,7 +41,13 @@ let db;
         ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Buddy', 'large'),
         ((SELECT user_id FROM Users WHERE username = 'emma123'), 'Luna', 'small'),
         ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Rocky', 'medium')`);
-      
+      await db.execute(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+        ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Buddy' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-11 14:00:00', 60, 'Central Park', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Luna' AND owner_id = (SELECT user_id FROM Users WHERE username = 'emma123')), '2025-06-12 10:00:00', 30, 'Riverside Walk', 'completed'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Rocky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-13 16:00:00', 45, 'Hilltop Trail', 'open')`);
+        
       }
   }
 
