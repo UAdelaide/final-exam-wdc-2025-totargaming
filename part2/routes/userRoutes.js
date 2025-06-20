b8f77a20306f6f7f39bc95b3afc3695b1b00ef5a
elvistran <elvistranhere>
2025-06-20 06:42:38 +0000

    [automatic commit] 2025-06-20 06:42:38 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -68,6 +68,14 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Login failed' });
   }
 });
-router
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Logout error:', err);
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.redirect('/index.html');
+  });
+});
 
 module.exports = router;
