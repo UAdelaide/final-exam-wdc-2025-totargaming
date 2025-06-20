f1a573095b8e2dc755ceb1beb2359b05ecb77987
elvistran <elvistranhere>
2025-06-20 02:37:19 +0000

    [automatic commit] 2025-06-20 02:37:19 UTC 193.115.102.250

--- part1/app.js
+++ part1/app.js
@@ -89,7 +89,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [requests] = await db.execute(`
-      SELECT wr.request_id, d.name as dog_name, u.username as owner_username, wr.requested_time, wr.duration_minutes, wr.location
+      SELECT wr.request_id, d.name as dog_name, wr.requested_time, wr.duration_minutes, wr.location
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
