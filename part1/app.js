c6df88ef8901de2c21015c76f814d48f149efdf1
elvistran <elvistranhere>
2025-06-20 02:29:40 +0000

    [automatic commit] 2025-06-20 02:29:40 UTC 193.115.102.250

--- part1/app.js
+++ part1/app.js
@@ -55,7 +55,7 @@ let db;
         ((SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Bella') AND status = 'accepted'), (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username = 'carol123'), 4, 'Great service')`);
       }
   } catch (error) {
-    
+    console.error('Database connection failed:', error);
   }
 
 })
