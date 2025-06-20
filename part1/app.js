3654a75975fab535fd6e97bc1e5f48c26beeb4df
elvistran <elvistranhere>
2025-06-20 02:24:14 +0000

    [automatic commit] 2025-06-20 02:24:14 UTC 193.115.102.250

--- part1/app.js
+++ part1/app.js
@@ -29,7 +29,12 @@ let db;
     });
     const [userCount] = await db.execute('SELECT COUNT(*) AS count FROM users');
     if (userCount[0].count === 0)  {
-      await db.execute()
+      await db.execute(`INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('davidwalker', 'david@example.com', 'hashed101', 'walker'),
+        ('emma123', 'emma@example.com', 'hashed202', 'owner')`);
     }
   }
 
