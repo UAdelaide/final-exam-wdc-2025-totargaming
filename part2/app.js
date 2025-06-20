8b0658f2cf7f62c462b04c5f7500196c5da11f38
elvistran <elvistranhere>
2025-06-20 07:50:27 +0000

    [automatic commit] 2025-06-20 07:50:27 UTC

--- part2/app.js
+++ part2/app.js
@@ -20,6 +20,6 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
+app.get()
 // Export the app instead of listening here
 module.exports = app;
