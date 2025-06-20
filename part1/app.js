be968a86177b70ac0a84f9fc0897beeeaeeca5bb
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 11:08:52 +0000

    [automatic commit] 2025-06-20 11:08:51 UTC

--- part1/app.js
+++ part1/app.js
@@ -91,7 +91,9 @@ app.get("/api/walkrequests/open", async (req, res) => {
 app.get("/api/walkers/summary", async (req, res) => {
   try {
     const [summary] = await db.execute(`
-      
+      SELECT
+    u.username                                   AS walker_username,
+
 
     `);
     res.json(summary);
