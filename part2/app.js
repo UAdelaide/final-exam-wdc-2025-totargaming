42dc268573f60088e268ada2b2b03e7069179a17
elvistran <elvistranhere>
2025-06-20 05:48:30 +0000

    [automatic commit] 2025-06-20 05:48:30 UTC

--- part2/app.js
+++ part2/app.js
@@ -6,9 +6,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'defaultsecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
