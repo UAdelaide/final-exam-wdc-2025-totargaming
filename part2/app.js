d916d667556fa5483eaab4ff4ef7d2567b152854
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 00:31:43 +0000

    [automatic commit] 2025-06-21 00:31:43 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-// added express-session for session management
-const session = require('express-session');
+
 const db = require('./models/db');
 const app = express();
+
+// added express-session for session management
+const session = require('express-session');
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, '/public')));
+
 
 
 // Configured and enabled the session middleware
